479b68a699e81585eecdcdbfac8f975490db66ee
Chris Hatch <hatch@tuta.io>
2017-06-05 16:27:16 +1000

    split Account into Container and View extract some strings to properties

--- src/components/Account.js
+++ src/components/Account.js
@@ -5,91 +5,91 @@ import {server as stellar} from '../lib/Stellar'
 import TransactionTable from './TransactionTable'
 import Asset from './shared/Asset'
 
-const Balances = (props) => {
-    const bals = props.balances.map((bal) =>
-        <div key={bal.asset_type}>
-            <Asset type={bal.asset_type}
-                   code={bal.asset_code}
-                   issuer={bal.asset_issuer} />
-            <div><FormattedMessage id="balance"/>: {bal.balance}</div>
-            <div>Limit: {bal.limit}</div>
-            <br/>
-        </div>
-    )
-    return (
-        <div>
-            <h4><FormattedMessage id="balances"/></h4>
-            {bals}
-        </div>
-    )
-}
+const Balance = (bal) =>
+    <div key={bal.asset_type}>
+        <Asset type={bal.asset_type}
+               code={bal.asset_code}
+               issuer={bal.asset_issuer} />
+        <div><FormattedMessage id="balance"/>: {bal.balance}</div>
+        <div><FormattedMessage id="limit"/>: {bal.limit}</div>
+    </div>
 
-const Thresholds = (props) => {
-    return (
-        <div>
-            <h4>Thresholds</h4>
-            <div>Low: {props.thresholds.low_threshold}</div>
-            <div>Med: {props.thresholds.med_threshold}</div>
-            <div>High: {props.thresholds.high_threshold}</div>
-            <br/>
-        </div>
-    )
-}
+const Balances = (props) =>
+    <div>
+        <h4><FormattedMessage id="balances"/></h4>
+        {props.balances.map(Balance)}
+    </div>
 
-const Signers = (props) => {
-    const signers = props.signers.map((signer) =>
-        <div key={signer.public_key}>
-            <div>Key: {signer.public_key}</div>
-            <div>Weight: {signer.weight}</div>
-            <br/>
-        </div>
-    )
-    return (
-        <div>
-            <h4>Signers</h4>
-            {signers}
-        </div>
-    )
+const Thresholds = (props) =>
+    <div>
+        <h4><FormattedMessage id="thresholds"/></h4>
+        <div>Low: {props.thresholds.low_threshold}</div>
+        <div>Med: {props.thresholds.med_threshold}</div>
+        <div>High: {props.thresholds.high_threshold}</div>
+    </div>
+
+const Signer = (signer) =>
+    <div key={signer.public_key}>
+        <div>Key: {signer.public_key}</div>
+        <div>Weight: {signer.weight}</div>
+    </div>
+
+const Signers = (props) =>
+    <div>
+        <h4><FormattedMessage id="signers"/></h4>
+        {props.signers.map(Signer)}
+    </div>
+
+class AccountContainer extends React.Component {
+  state = {
+      account: null
+  }
+
+  componentDidMount() {
+      this.loadAccount(this.props.match.params.id)
+  }
+
+  componentWillReceiveProps(nextProps){
+      this.loadAccount(nextProps.match.params.id)
+  }
+
+  loadAccount(accountId) {
+      stellar.accounts().accountId(accountId).call().then((res) => {
+          this.setState({account: res})
+      })
+  }
+
+  render() {
+      return (this.state.account === null)
+          ? null
+          : <Account account={this.state.account}/>
+  }
 }
 
 class Account extends React.Component {
-    componentDidMount() {
-        this.loadAccount(this.props.match.params.id)
-    }
-
-    componentWillReceiveProps(nextProps){
-        this.loadAccount(nextProps.match.params.id)
-    }
-
-    loadAccount(accountId) {
-        stellar.accounts().accountId(accountId).call().then((res) => {
-            this.setState({account: res})
-        })
-    }
-
     render() {
-        if (this.state === null || this.state.account === null)
-            return null
-
-        const a = this.state.account
-        // const {formatMessage} = this.props.intl
-
+        const a = this.props.account
         return (
             <Grid>
                 <Row>
                     <div><FormattedMessage id="account"/> {a.id}</div>
-                    <div>Sequence {a.sequence}</div>
-                    <br/>
+                    <div><FormattedMessage id="sequence"/> {a.sequence}</div>
+                </Row>
+                <Row>
                     <Balances balances={a.balances}/>
+                </Row>
+                <Row>
                     <Thresholds thresholds={a.thresholds}/>
+                </Row>
+                <Row>
                     <Signers signers={a.signers}/>
                 </Row>
                 <Row>
-                        <TransactionTable account={a.id} limit={10}/>
+                    <TransactionTable account={a.id} limit={10}/>
                 </Row>
             </Grid>
         )
     }
 }
 
-export default Account
+export default AccountContainer
