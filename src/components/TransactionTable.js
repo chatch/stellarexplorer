9908f1ef0eeddcaf689b7f5409da58d35874efe1
Chris Hatch <hatch@tuta.io>
2017-06-08 13:01:31 +1000

    add paging to transactions table

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -3,33 +3,42 @@ import {Table} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import {FormattedRelative, FormattedMessage} from 'react-intl'
 import {server as stellar} from '../lib/Stellar'
-import {withSpinner} from './shared/HOCs'
+import {withPaging, withSpinner} from './shared/HOCs'
 import {isDefInt, isAccount, shortHash} from '../lib/Utils'
-
-const REFRESH_RATE = 15000
-const DEFAULT_LIMIT = 5
 
 const responseToTxs = (rsp) => {
   return rsp.records.map((tx) => {
-    return {hash: tx.hash, time: tx.created_at, opCount: tx.operation_count, ledger: tx.ledger_attr}
+    const rec = {
+      hash: tx.hash,
+      time: tx.created_at,
+      opCount: tx.operation_count,
+      ledger: tx.ledger_attr
+    }
+    return rec
   })
 }
 
-const isLoading = (props) => (props.isLoading === true)
-
 class TransactionRow extends React.Component {
-  render() {
+  renderTxHash() {
     const txHash = this.props.hash
-    const hashLabel = (this.props.compact === true)
+    const compact = (this.props.compact === true)
+    const hashLabel = (compact)
       ? shortHash(txHash)
       : txHash
+    const className = (!compact)
+      ? "monospace"
+      : ""
+    return (
+      <span title={txHash} className={className}>
+        <Link to={`/tx/${txHash}`}>{hashLabel}</Link>
+      </span>
+    )
+  }
+
+  render() {
     return (
       <tr>
-        <td>
-          <span title={txHash} className={"monospace"}>
-            <Link to={`/tx/${txHash}`}>{hashLabel}</Link>
-          </span>
-        </td>
+        <td>{this.renderTxHash()}</td>
         <td><FormattedRelative value={this.props.time}/></td>
         <td>{this.props.opCount}</td>
         <td>
@@ -41,8 +50,6 @@ class TransactionRow extends React.Component {
 }
 
 class TransactionTable extends React.Component {
-  renderRow(tx) {}
-
   render() {
     return (
       <Table
@@ -65,11 +72,19 @@ class TransactionTable extends React.Component {
     )
   }
 }
-const WrappedTransactionTable = withSpinner(TransactionTable, isLoading)
+
+const loadingCondition = (props) => props.isLoading === true
+const pagingCondition = (props) => props.paging === true
+
+const TransactionTableWithPaging = withPaging(TransactionTable, pagingCondition)
+const TransactionTableWithAll = withSpinner(TransactionTableWithPaging, loadingCondition)
 
 class TransactionTableContainer extends React.Component {
   static defaultProps = {
-    compact: true
+    compact: true,
+    limit: 5,
+    paging: false,
+    refreshRate: 15000
   }
 
   state = {
@@ -78,48 +93,84 @@ class TransactionTableContainer extends React.Component {
   }
 
   componentDidMount() {
-    this.update()
-    this.timerID = setInterval(() => this.update(), REFRESH_RATE);
+    this.updateWithLatest()
+    if (this.props.paging === false) { // don't refresh data on paging views
+      this.timerID = setInterval(() => this.updateWithLatest(), this.props.refreshRate);
+    }
   }
 
   componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  update() {
-    this.setState({isLoading: true, txs: []})
-    this.transactions(this.props).then((stellarRsp) => {
-      this.setState({txs: responseToTxs(stellarRsp), isLoading: false})
-    }).catch((err) => {
-      console.error(`Failed to fetch transactions: [${err}]`)
-      this.setState({txs: [], isLoading: false})
-    })
+    if (this.timerID) {
+      clearInterval(this.timerID);
+      delete this.timerID
+    }
   }
 
   transactions(props) {
     const builder = stellar.transactions()
-
     if (isDefInt(props, 'ledger'))
       builder.forLedger(props.ledger)
-
     if (isAccount(props.account))
       builder.forAccount(props.account)
-
-    const limit = (isDefInt(props, 'limit'))
-      ? props.limit
-      : DEFAULT_LIMIT
-    builder.limit(limit)
-
+    builder.limit(props.limit)
     builder.order('desc')
-
     return builder.call()
   }
 
+  updateWithLatest() {
+    this.update(this.transactions(this.props))
+  }
+
+  update(txsPromise, reverse = false) {
+    console.log(`updating table`)
+    this.setState({isLoading: true, txs: []})
+    txsPromise.then((rsp) => {
+      const newState = {
+        txs: responseToTxs(rsp),
+        isLoading: false,
+        next: (reverse === true)
+          ? rsp.prev
+          : rsp.next,
+        prev: (reverse === true)
+          ? rsp.next
+          : rsp.prev
+      }
+      const nt = newState.txs
+      if (reverse === true) {
+        console.log(`reverse`);
+        newState.txs = newState.txs.reverse()
+      }
+      console.log(`new: ledger: first: ${nt[0].time}|${nt[0].ledger} last: ${nt[nt.length - 1].time}|${nt[nt.length - 1].ledger}`)
+      this.setState(newState)
+    }).catch((err) => {
+      console.error(`Failed to fetch transactions: [${err.stack}]`)
+      this.setState({txs: [], isLoading: false})
+    })
+  }
+
+  handleClickNext = () => {
+    this.changePage(this.state.next)
+  }
+
+  handleClickPrev = () => {
+    this.changePage(this.state.prev, true)
+  }
+
+  changePage = (pageFunc, reverse = false) => {
+    if (!pageFunc) {
+      console.error(`pageFunc not defined`)
+      return
+    }
+    this.update(pageFunc(), reverse)
+  }
+
   render() {
-    return (<WrappedTransactionTable
+    return (<TransactionTableWithAll
       isLoading={this.state.isLoading}
       txs={this.state.txs}
-      compact={this.props.compact}/>)
+      handleClickNext={this.handleClickNext}
+      handleClickPrev={this.handleClickPrev}
+      {...this.props}/>)
   }
 }
 
--- src/components/shared/HOCs.js
+++ src/components/shared/HOCs.js
@@ -1,15 +1,46 @@
 import React from 'react'
+import {Pager} from 'react-bootstrap'
 
 import Spinner from './Spinner'
 
-function withSpinner(WrappedComponent, conditionalRenderingFn) {
+function withEither(OnTrueComponent, OnFalseComponent, conditionalRenderingFn) {
   return class extends React.Component {
     render() {
       return conditionalRenderingFn(this.props)
-        ? <Spinner/>
-        : <WrappedComponent { ...this.props }/>
+        ? <OnTrueComponent { ...this.props }/>
+        : <OnFalseComponent { ...this.props }/>
     }
   }
 }
 
-export {withSpinner}
+function withSpinner(WrappedComponent, conditionalRenderingFn) {
+  return withEither(Spinner, WrappedComponent, conditionalRenderingFn)
+}
+
+function withPaging(WrappedComponent, conditionalRenderingFn) {
+  return class extends React.Component {
+    render() {
+      if (conditionalRenderingFn(this.props) === false)
+        return (<WrappedComponent {...this.props}/>)
+      return (
+        <div>
+          <Paging
+            handleClickNext={this.props.handleClickNext}
+            handleClickPrev={this.props.handleClickPrev}/>
+          <WrappedComponent {...this.props}/>
+        </div>
+      )
+    }
+  }
+}
+
+function Paging(props) {
+  return (
+    <Pager>
+      <Pager.Item previous onClick={props.handleClickPrev} href="#">&larr; Previous Page</Pager.Item>
+      <Pager.Item next onClick={props.handleClickNext} href="#">Next Page &rarr;</Pager.Item>
+    </Pager>
+  )
+}
+
+export {withSpinner, withPaging}
