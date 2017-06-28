153cb9b6df8b93deb33cba9d4d5036d20da5506a
Chris Hatch <hatch@tuta.io>
2017-06-29 06:49:07 +1000

    style the link in TitleWithLink so it's more visible

--- src/components/shared/TitleWithLink.js
+++ src/components/shared/TitleWithLink.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const linkStyle = {color: 'white', textDecoration: 'underline'}
+
 const TitleWithLink = ({title, rightLinkLabel, rightLinkAddr}) =>
   <div>
     <span>{title}</span>
-    <a href={rightLinkAddr} className="pull-right">{rightLinkLabel}</a>
+    <a href={rightLinkAddr} className="pull-right" style={linkStyle}>
+      {rightLinkLabel}
+    </a>
   </div>
 
 TitleWithLink.propTypes = {
