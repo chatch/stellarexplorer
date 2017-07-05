f1949ec923a0371c3d8013057e9155d067962da0
Chris Hatch <hatch@tuta.io>
2017-07-05 15:50:33 +1000

    mock out XHR for jest test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import './__mocks__/MockXHR.js'
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
