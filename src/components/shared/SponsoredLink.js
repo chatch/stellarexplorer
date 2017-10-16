276251b61e4b2c4b62ef34e57bc29ff57f9eb40d
Chris Hatch <hatch@tuta.io>
2017-10-16 22:57:46 +0700

    don't render an empty banner message

--- src/components/shared/SponsoredLink.js
+++ src/components/shared/SponsoredLink.js
@@ -3,27 +3,29 @@ import Col from 'react-bootstrap/lib/Col'
 import Grid from 'react-bootstrap/lib/Grid'
 import Row from 'react-bootstrap/lib/Row'
 import FetchPonyfill from 'fetch-ponyfill'
+import has from 'lodash/has'
+import isEmpty from 'lodash/isEmpty'
 
 const fetch = FetchPonyfill().fetch
 
 const SPONSOR_LINK_JSON =
   'https://raw.githubusercontent.com/chatch/stellarexplorer/master/banner.json'
 
-const SponsoredLink = ({link}) =>
+const SponsoredLink = ({message}) => (
   <Grid>
     <Row>
       <Col style={{marginBottom: 15, paddingLeft: 15}}>
-        Sponsored Link:{' '}
-        <span dangerouslySetInnerHTML={{__html: link.message}} />
+        Sponsored Link: <span dangerouslySetInnerHTML={{__html: message}} />
       </Col>
     </Row>
   </Grid>
+)
 
 class SponsoredLinkContainer extends React.Component {
   componentDidMount() {
     fetch(SPONSOR_LINK_JSON)
       .then(rsp => rsp.json())
-      .then(json => this.setState({link: json}))
+      .then(json => this.setState({message: json.message}))
       .catch(err => {
         console.error(`Failed to fetch sponsored link: [${err}]`)
         console.error(`stack: [${err.stack}]`)
@@ -31,8 +33,13 @@ class SponsoredLinkContainer extends React.Component {
   }
 
   render() {
-    if (!this.state) return null
-    return <SponsoredLink {...this.state} />
+    if (
+      !this.state ||
+      !has(this.state, 'message') ||
+      isEmpty(this.state.message)
+    )
+      return null
+    return <SponsoredLink message={this.state.message} />
   }
 }
 
