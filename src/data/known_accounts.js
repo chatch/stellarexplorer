3e54c1c03cfa2e218bc233c56689dc87c9882fe1
Chris Hatch <hatch@tuta.io>
2017-10-21 21:31:43 +0700

    add anchor domain to display name mapping so names appear in operations lists

--- src/data/known_accounts.js
+++ src/data/known_accounts.js
@@ -1,4 +1,27 @@
+import has from 'lodash/has'
 import {anchors, destinations} from 'stellarterm-directory'
+
+// TODO: remove this if PR https://github.com/irisli/stellarterm/pull/91
+// is merged as it adds display names to the directory
+const anchorDisplayNames = {
+  'naobtc.com': 'NaoBTC',
+  'tempo.eu.com': 'TEMPO',
+  'ripplefox.com': 'RippleFox',
+  'coins.asia': 'coins.asia',
+  'vcbear.net': 'VCBear',
+  'tonaira.com': 'Tonaira',
+  'apay.io': 'apay.io',
+  'btc.papayame.com': 'apay.io', // remove when removed from directory
+  'ltc.papayame.com': 'apay.io', // remove when removed from directory
+  'gft.network': 'GFT',
+  'collective21.org': 'Collective21',
+  'liquido.i-server.org': 'Liquido',
+  'equid.co': 'eQuid',
+  'moni.com': 'MONI',
+  'bitcoinfundi.com': 'Golix',
+  'cryptomover.com': 'Cryptomover',
+  'mobius.network': 'Mobius',
+}
 
 const knownAccounts = {}
 
@@ -10,6 +33,9 @@ Object.keys(anchors).forEach(domain => {
     const issuer = asset.substring(asset.indexOf('-') + 1)
     knownAccounts[issuer] = anchor
     knownAccounts[issuer].img = anchor.logo
+    knownAccounts[issuer].name = has(anchorDisplayNames, domain)
+      ? anchorDisplayNames[domain]
+      : domain
     knownAccounts[issuer].type = 'issuer'
   })
 })
