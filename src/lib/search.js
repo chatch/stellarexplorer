927c02f89954be34e51912f5bd4172e9919a2172
Chris Hatch <hatch@tuta.io>
2018-03-12 15:10:14 +0700

    #57 add basic resolve for search by asset code and anchor name

--- src/lib/search.js
+++ src/lib/search.js
@@ -1,25 +1,61 @@
 import isNaN from 'lodash/isNaN'
+import isEmpty from 'lodash/isEmpty'
 import isString from 'lodash/isString'
 import toNumber from 'lodash/toNumber'
 
 import {isPublicKey, isStellarAddress, isTxHash} from './utils'
+import directory from '../data/directory'
+
+const {anchors, assets} = directory
+
+const lcEquals = (str1, str2) =>
+  !isEmpty(str1) && !isEmpty(str2) && str1.toLowerCase() === str2.toLowerCase()
+
+const lcIncludes = (str1, str2) =>
+  !isEmpty(str1) &&
+  !isEmpty(str2) &&
+  str1.toLowerCase().includes(str2.toLowerCase())
+
+const searchAssetCode = code =>
+  Object.keys(assets)
+    .filter(key => lcEquals(assets[key].code, code))
+    .map(key => assets[key])
+
+const searchAnchorName = name =>
+  Object.keys(anchors).filter(
+    key =>
+      lcIncludes(anchors[key].name, name) ||
+      lcIncludes(anchors[key].displayName, name)
+  )
 
 const searchStrToPath = searchStr => {
   if (!isString(searchStr) || searchStr.trim() === '') return null
 
   const str = searchStr.trim()
 
-  let path = null
   if (isPublicKey(str) || isStellarAddress(str)) {
-    path = `/account/${str}`
+    return `/account/${str}`
   } else if (isTxHash(str)) {
-    path = `/tx/${str}`
+    return `/tx/${str}`
   } else if (!isNaN(toNumber(str))) {
-    path = `/ledger/${toNumber(str)}`
-  } else {
-    path = `/error/not-found/${searchStr}`
+    return `/ledger/${toNumber(str)}`
   }
-  return path
+
+  // search by asset code
+  const codeMatch = searchAssetCode(str)
+  if (codeMatch.length > 0) {
+    return `/account/${codeMatch[0].issuer}`
+  }
+
+  // search by anchor name (exact or substring)
+  const nameMatch = searchAnchorName(str)
+  if (nameMatch.length > 0) {
+    console.log(nameMatch[0])
+    return null
+    // return `/account/${nameMatch[0].issuer}`
+  }
+
+  return `/error/not-found/${searchStr}`
 }
 
 export {searchStrToPath}
