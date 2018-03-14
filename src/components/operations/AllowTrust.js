759f5596d04aba0a0a1caf0eb1e6072d05fb9d1f
Chris Hatch <hatch@tuta.io>
2018-03-14 18:34:36 +0700

    #36 add trustor to allow trust operation

--- src/components/operations/AllowTrust.js
+++ src/components/operations/AllowTrust.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Trust from './Trust'
 import {FormattedMessage} from 'react-intl'
+import AccountLink from '../shared/AccountLink'
+import Trust from './Trust'
 
 const AllowTrust = props => (
   <Trust {...props}>
@@ -8,6 +9,7 @@ const AllowTrust = props => (
       id="operation.trust.allow"
       values={{
         authorize: String(props.authorize),
+        trustor: <AccountLink account={props.trustor} />,
       }}
     />
   </Trust>
