4beb35e3a1d5c2c3192e7d78a18b238c5a965236
Chris Hatch <hatch@tuta.io>
2018-03-15 10:47:16 +0700

    #61 reduce new window icon size down a little

--- src/components/shared/NewWindowIcon.js
+++ src/components/shared/NewWindowIcon.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Glyphicon from 'react-bootstrap/lib/Glyphicon'
 
 const NewWindowIcon = () => (
-  <Glyphicon glyph="new-window" style={{fontSize: 'smaller', marginLeft: 5}} />
+  <Glyphicon glyph="new-window" style={{fontSize: 11, marginLeft: 5}} />
 )
 
 export default NewWindowIcon
