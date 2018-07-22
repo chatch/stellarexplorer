623db07f0e299f60ad5d8cb6693ed46eaf3d8b4c
Chris Hatch <hatch@tuta.io>
2018-07-22 10:55:08 +0700

    disable LumensRates for now

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Grid from 'react-bootstrap/lib/Grid'
 import Row from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
-import LumensRates from '../shared/LumensRates'
+// import LumensRates from '../shared/LumensRates'
 
 class Footer extends React.PureComponent {
   render() {
@@ -10,7 +10,9 @@ class Footer extends React.PureComponent {
       <Grid id="footer">
         <Row>
           <Col md={3}>
-            <LumensRates />
+            {
+              // Disable rates for now, it doesn't refresh correctly: <LumensRates/>
+            }
           </Col>
           <Col mdOffset={7} md={2}>
             <a href="https://github.com/chatch/stellarexplorer">
