820a357d761ac3384f25366c52a0b260f85c43ba
vladidalv yaroslavtsev <vlad@users.noreply.github.com>
2021-10-22 11:23:46 -0400

    comment unregister lines

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App'
 
 // TODO: Re-enable service worker after renaming the PWA and updating its icon (see manifest.json)
 import registerServiceWorker from './registerServiceWorker'
-import { unregister as unregisterServiceWorker } from './registerServiceWorker';
+// import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker();
 
-unregisterServiceWorker()
+// unregisterServiceWorker()
