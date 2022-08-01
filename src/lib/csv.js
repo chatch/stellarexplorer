eae9916243955d6a51aae6306f38712752c4d2fe
simontrinh0911 <93080579+simontrinh0911@users.noreply.github.com>
2022-08-01 09:38:40 +0200

    Update csv.js

--- src/lib/csv.js
+++ src/lib/csv.js
@@ -21,7 +21,6 @@ const jsonToCSV = records => {
       columns.push(...newKeys)
     }
   })
-
   // map col name to col idx
   const colToIdx = columns.reduce((accumulated, curCol, curIdx) => {
     accumulated[curCol] = curIdx
@@ -31,25 +30,4 @@ const jsonToCSV = records => {
   const numCols = columns.length
   const headerCsv = toCsvString(columns)
 
-  return records.reduce((csvStr, curRec) => {
-    const row = new Array(numCols)
-    row.fill('')
-
-    Object.keys(curRec).forEach(key => (row[colToIdx[key]] = curRec[key]))
-    csvStr = `${csvStr}\n${toCsvString(row)}`
-
-    return csvStr
-  }, headerCsv)
-}
-
-const exportCSV = records => {
-  const csvData = jsonToCSV(records)
-  const autoByteOrderMark = true
-  saveAs(
-    new Blob(['\ufeff', csvData], {type: 'text/csv;charset=utf-8'}),
-    'pi-blockexplorer-export.csv',
-    autoByteOrderMark
-  )
-}
-
-export {exportCSV, jsonToCSV}
+	
