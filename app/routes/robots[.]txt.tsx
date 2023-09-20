cf0e677717604d67af7119c1bfd0ac4f0c3c48c1
Chris Hatch <hatch@tuta.io>
2023-09-20 10:12:05 +0700

    robots.txt take 2: turns out you need a route for it to work with the deployed server

---
+++ app/routes/robots[.]txt.tsx
@@ -0,0 +1,11 @@
+export const loader = () => {
+  const robotText = `User-agent: *
+Disallow:
+`
+  return new Response(robotText, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    }
+  })
+}
