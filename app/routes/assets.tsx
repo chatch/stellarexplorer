277da6b53f0810bfbb92cc46073f518b8681d6c3
Ryuji Eguchi <jp.ryuji@gmail.com>
2024-01-16 15:18:47 +1100

    update asset page

--- app/routes/assets.tsx
+++ app/routes/assets.tsx
@@ -3,14 +3,23 @@ import { useEffect } from 'react'
 import { Assets } from './lib/assets-base'
 import { setTitle } from '../lib/utils'
 
-import directory from '../data/directory'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 
-const { assets } = directory
+export async function loader() {
+  const response = await fetch(
+    'https://api.stellar.expert/explorer/public/asset?sort=volume7d&order=desc&limit=50',
+  )
+  const assets = await response.json()
+  return json(assets._embedded.records)
+}
 
 export default function AssetsAll() {
+  const assets = useLoaderData<typeof loader>()
+
   useEffect(() => {
     setTitle('Assets')
   }, [])
 
-  return <Assets assetKeys={Object.keys(assets)} />
+  return <Assets assets={assets} />
 }
--- app/routes/lib/assets-base.tsx
+++ app/routes/lib/assets-base.tsx
@@ -5,56 +5,135 @@ import CardHeader from 'react-bootstrap/CardHeader'
 import Row from 'react-bootstrap/Row'
 import Table from 'react-bootstrap/Table'
 import { FormattedMessage, useIntl } from 'react-intl'
-import { Link } from 'react-router-dom'
+import svgCircle from '../../../public/img/circle.svg'
 
 import { setTitle } from '../../lib/utils'
-import AccountLink from '../../components/shared/AccountLink'
 import BackendResourceBadgeButton from '../../components/shared/BackendResourceBadgeButton'
-import ClipboardCopy from '../../components/shared/ClipboardCopy'
-import Logo from '../../components/shared/Logo'
 import NewWindowIcon from '../../components/shared/NewWindowIcon'
 import { TitleWithJSONButton } from '~/components/shared/TitleWithJSONButton'
+import styles from './assets-base.module.css'
+import { formatPercentToHumanReadable, formatPrice24h } from '~/lib/utilities'
 
-import directory from '../../data/directory'
-
-const { anchors, assets } = directory
 export const METADATA_URI =
   'https://raw.githubusercontent.com/irisli/stellarterm/master/directory/directory.json'
 
 export interface AssetProps {
-  code: string
-  issuer: string
-  domain: string
+  asset: string
+  supply: number
+  traded_amount: number
+  payments_amount: number
+  created: number
+  trustlines: number[]
+  payments: number
+  domain?: string
+  rating: {
+    age: number
+    trades: number
+    payments: number
+    trustlines: number
+    volume7d: number
+    interop: number
+    liquidity: number
+    average: number
+  }
+  price7d: number[][]
+  volume7d: number
+  tomlInfo?: {
+    code?: string
+    issuer?: string
+    name?: string
+    image?: string
+    anchorAssetType?: string
+    anchorAsset?: string
+    orgName?: string
+    orgLogo?: string
+  }
+  paging_token: number
 }
 
-export function Asset({ code, domain, issuer }: AssetProps) {
-  const anchor = anchors[domain]
+const getAssetImage = (asset: AssetProps) => {
+  if (asset.tomlInfo?.image) {
+    return asset.tomlInfo.image
+  }
+  if (asset.asset.startsWith('XLM') || asset.asset.startsWith('yXLM')) {
+    return 'https://ipfs.io/ipfs/bafkreihntcz2lpaxawmbhwidtuifladkgew6olwuly2dz5pewqillhhpay'
+  }
+  return svgCircle
+}
+
+const getAssetCode = (asset: AssetProps) => {
+  if (asset.tomlInfo?.code) {
+    return asset.tomlInfo.code
+  }
+  return asset.asset.split('-')[0]
+}
+
+export function Asset(asset: AssetProps) {
+  const displayDate = asset.created
+    ? new Date(asset.created * 1000).toLocaleDateString('sv-SE')
+    : ''
+  const holders = ` ${asset.trustlines[
+    asset.trustlines.length - 1
+  ].toLocaleString()}/${asset.trustlines[0].toLocaleString()}`
+  const payments = asset.payments.toLocaleString()
+  const supply = (asset.supply / 10_000_000).toLocaleString(undefined, {
+    maximumFractionDigits: 0,
+  })
+  // NOTE: asset.price7d seems to have a maximum of 8 elements, each of which is an array of 2 elements.
+  // And the elements are timestamp and price. And they are sorted in ascending order by timestamp.
+  // So the last one is the most recent one.
+  const latestPrice = asset.price7d[asset.price7d.length - 1][1]
+  let previousPrice = null
+  if (asset.price7d.length > 1) {
+    previousPrice = asset.price7d[asset.price7d.length - 2][1] ?? null
+  }
+  const price24h = formatPrice24h(latestPrice)
+  const priceChange = previousPrice
+    ? formatPercentToHumanReadable(
+        Math.abs(((latestPrice - previousPrice) * 100) / latestPrice),
+      )
+    : undefined
+
   return (
     <tr className="directoryRow">
       <td>
-        <a href={anchor.website || ''} target="_blank" rel="noreferrer">
-          <Logo name={domain} type="anchor" />
-        </a>
+        <img
+          src={getAssetImage(asset)}
+          alt={getAssetCode(asset)}
+          className={styles['asset-image']}
+        />
+        <span className={styles['asset-code']}>{getAssetCode(asset)}</span>
       </td>
-      <td style={{ color: 'white' }}>{code}</td>
-      <td>
-        <AccountLink account={issuer} hideKnown />
-        <ClipboardCopy text={issuer} />
+      <td>{displayDate}</td>
+      <td className={styles['assets-cell']}>{supply}</td>
+      <td className={styles['assets-cell']}>{holders}</td>
+      <td className={styles['assets-cell']}>{payments}</td>
+      <td className={styles['assets-cell']}>
+        {price24h}
+        {previousPrice && latestPrice >= previousPrice ? (
+          <span className={`${styles['price-change-up']}`}>
+            {priceChange && `↑${priceChange}%`}
+          </span>
+        ) : (
+          <span className={`${styles['price-change-down']}`}>
+            {priceChange && `↓${priceChange}%`}
+          </span>
+        )}
+        <span className={`${styles['text-small']}`}>USD</span>
       </td>
       <td>
         <div>
-          <Link to={`/anchor/${domain}`}>{anchor.name}</Link>
-        </div>
-        <div>
-          <a href={anchor.website} target="_blank" rel="noreferrer">
-            {anchor.website}
-            <NewWindowIcon />
+          <a href={`https://${asset.domain}`} target="_blank" rel="noreferrer">
+            {asset.domain}
+            {asset.domain && <NewWindowIcon />}
           </a>
         </div>
+      </td>
+      <td>
         <div className="stellarToml">
           <BackendResourceBadgeButton
             label="server.toml"
-            url={`https://${domain}/.well-known/stellar.toml`}
+            url={`https://${asset.domain}/.well-known/stellar.toml`}
           />
         </div>
       </td>
@@ -62,8 +141,9 @@ export function Asset({ code, domain, issuer }: AssetProps) {
   )
 }
 
-export function Assets({ assetKeys }: { assetKeys: Array<string> }) {
+export function Assets({ assets }: { assets: AssetProps[] }) {
   const { formatMessage } = useIntl()
+
   useEffect(() => {
     setTitle('Assets')
   }, [])
@@ -82,22 +162,31 @@ export function Assets({ assetKeys }: { assetKeys: Array<string> }) {
             <Table id="assets-table">
               <thead>
                 <tr>
-                  <th />
                   <th>
-                    <FormattedMessage id="code" />
+                    <FormattedMessage id="asset" />
                   </th>
                   <th>
-                    <FormattedMessage id="issuer" />
+                    <FormattedMessage id="created" />
                   </th>
-                  <th>
-                    <FormattedMessage id="anchor" />
+                  <th className={styles['assets-cell']}>
+                    <FormattedMessage id="supply" />
                   </th>
+                  <th className={styles['assets-cell']}>
+                    <FormattedMessage id="holders" />
+                  </th>
+                  <th className={styles['assets-cell']}>
+                    <FormattedMessage id="payments" />
+                  </th>
+                  <th className={styles['assets-cell']}>
+                    <FormattedMessage id="price(24h)" />
+                  </th>
+                  <th>URL</th>
+                  <th>server.toml</th>
                 </tr>
               </thead>
               <tbody>
-                {assetKeys.sort().map((key) => {
-                  const asset = assets[key]
-                  return <Asset key={key} {...asset} />
+                {assets.map((asset) => {
+                  return <Asset key={asset.asset} {...asset} />
                 })}
               </tbody>
             </Table>
